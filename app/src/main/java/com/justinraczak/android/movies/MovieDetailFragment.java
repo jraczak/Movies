7ab0cf73a869bd940638f5e23046d86fe0f4e2ab
Justin <jraczak@gmail.com>
2016-06-22 18:57:06 -0700

    Small updates to fix phone view

--- app/src/main/java/com/justinraczak/android/movies/MovieDetailFragment.java
+++ app/src/main/java/com/justinraczak/android/movies/MovieDetailFragment.java
@@ -58,6 +58,7 @@ public class MovieDetailFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(LOG_TAG, "In onCreate");
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
