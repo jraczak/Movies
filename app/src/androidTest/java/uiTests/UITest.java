17dac21c4883e2170a9050ecf0adee316646ddac
Justin Raczak <jraczak@gmail.com>
2016-08-25 19:23:58 -0700

    Move maven detail to app module build.gradle

--- app/src/androidTest/java/uiTests/UITest.java
+++ app/src/androidTest/java/uiTests/UITest.java
@@ -4,6 +4,27 @@ package uiTests;
  * Created by justinr on 8/25/16.
  */
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import com.xamarin.testcloud.appium.Factory;
+import com.xamarin.testcloud.appium.EnhancedAndroidDriver;
+import org.junit.rules.TestWatcher;
+import org.junit.Rule;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.remote.http.HttpClient;
+
+import static org.junit.Assert.*;
 
 public class UITest {
 
